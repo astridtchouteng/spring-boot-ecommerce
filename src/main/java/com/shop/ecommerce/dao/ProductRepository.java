2b45b5163e849c0e66437dc12f401937d1d2c4ca
astridtchouteng <astrid.tchouteng@gmail.com>
2020-12-30 01:38:05 +0100

    exposes endpoint for find product by category

--- src/main/java/com/shop/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/shop/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.shop.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shop.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	/*
+	 * Spring Data REST automatically exposes endpoints for us
+	 * http://localhost:8282/api/products/search/findByCategoryId?id:id
+	 * */
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
